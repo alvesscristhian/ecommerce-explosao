e4fc42b692610e36ace8531b506dfebdd6896cd5
Cristhian Alves <cristhian.ads@hotmail.com>
2025-12-31 00:04:17 -0300

    feat: Navbar concluida

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/views/**/*.ejs",
-    "./src/**/*.{js,ts,jsx,tsx}"
+    "./src/views/**/*.ejs"
   ],
   theme: {
     extend: {},
